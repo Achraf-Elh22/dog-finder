a42e49c87f2ea0bb3882cf07f1dff6ebd105baf7
achraf elhamzaoui <achrafelh22@gmail.com>
2020-04-03 12:47:23 +0100

    Finish building simple web page using React and  React-route-app

---
+++ src/Routes.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import DogDetails from './DogDetails';
+import DogList from './DogList';
+
+export default class Routes extends Component {
+  render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        d => d.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
+    return (
+      <Switch>
+        <Route
+          exact
+          path='/dogs'
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route path='/dogs/:name' render={getDog} />
+        <Redirect to='/dogs' />
+      </Switch>
+    );
+  }
+}
